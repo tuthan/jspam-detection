8c66230ae932d73b1acf175432aafe7fa10a964f
luongvinhthao <luongvinhthao@8fc22f1e-72e6-7597-62d5-0bd44c5c74ef>
2011-01-18 11:36:16 +0000

    UPDATE phan tao vong loop bat goi tin

--- jspam-detection/src/jspamdetection/Main.java
+++ jspam-detection/src/jspamdetection/Main.java
@@ -10,17 +10,35 @@ package jspamdetection;
  * @author jspamdetection team
  */
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.NetworkInterface;
  import java.util.ArrayList;
  import java.util.Date;
  import java.util.List;
  import java.lang.*;
- import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.NetworkInterface;
  import org.jnetpcap.Pcap;
  import org.jnetpcap.PcapIf;
  import org.jnetpcap.packet.PcapPacket;
  import org.jnetpcap.packet.PcapPacketHandler;
+ import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jnetpcap.JBufferHandler;
+import org.jnetpcap.JCaptureHeader;
+import org.jnetpcap.Pcap;
+import org.jnetpcap.PcapHeader;
+import org.jnetpcap.PcapIf;
+import org.jnetpcap.nio.JBuffer;
+import org.jnetpcap.nio.JMemory;
+import org.jnetpcap.packet.JPacket;
+import org.jnetpcap.packet.JPacketHandler;
+import org.jnetpcap.protocol.lan.Ethernet;
+import org.jnetpcap.protocol.tcpip.Tcp;
+import sun.nio.ch.SocketOpts;
+import sun.nio.ch.SocketOpts.IP.TCP;
+
 /**
  *
  * @author LVT
@@ -72,12 +90,44 @@ public class Main {
          PcapIf netInterface = alldevice.get(I);
          System.out.println(alldevice.get(I).getDescription());
          //open nic  to capture packet
-         int snaplen =2048;
+         int snaplen =64*2048;
          int promicious= Pcap.MODE_PROMISCUOUS;
-         int timeout= 60*1000;
+         int timeout= 10*1000;
          Pcap pcap= Pcap.openLive(netInterface.getName(), snaplen, promicious, timeout, err);
+         if(pcap==null)
+         {
+            System.out.println("khong the mo card mang");
+         }
+         // vong loop bat goi tin
 
 
-    }
 
+   JBufferHandler<String> printSummaryHandler = new JBufferHandler<String>()
+   {
+
+	public void nextPacket(PcapHeader header, JBuffer buffer, String user)
+        {
+	Timestamp timestamp =  new Timestamp(header.timestampInMillis());
+        final PcapPacket packet = new PcapPacket(JMemory.POINTER);
+	 packet.peer(buffer);
+         packet.getCaptureHeader().peerTo(header, 0);
+         packet.scan(Ethernet.ID);
+         Tcp tcp = new Tcp();
+         if (packet.hasHeader(tcp))
+         {
+             System.out.println(packet.getCaptureHeader());
+         }
+        // System.out.printf("Received packet at %s caplen=%-4d len=%-4d %s\n",
+	//			    timestamp.toString(), // timestamp to 1 ms accuracy
+	//			    header.caplen(), // Length actually captured
+	//			    header.wirelen(), // Original length of the packet
+	//			    user // User supplied object
+	//			    );
+			}
+		};
+                // dat vong loop la 10 packet
+		pcap.loop(10, printSummaryHandler, "jNetPcap rocks!");
+                // dong pcap
+		pcap.close();
+}
 }
