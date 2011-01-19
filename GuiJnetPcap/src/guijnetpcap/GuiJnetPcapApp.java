01944aca8a82132c87a0bacaa461cc3fb1df771e
luongvinhthao <luongvinhthao@8fc22f1e-72e6-7597-62d5-0bd44c5c74ef>
2011-01-19 14:39:47 +0000

    Gui cua Jnetpcap

---
+++ GuiJnetPcap/src/guijnetpcap/GuiJnetPcapApp.java
@@ -0,0 +1,44 @@
+/*
+ * GuiJnetPcapApp.java
+ */
+
+package guijnetpcap;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class GuiJnetPcapApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new GuiJnetPcapView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of GuiJnetPcapApp
+     */
+    public static GuiJnetPcapApp getApplication() {
+        return Application.getInstance(GuiJnetPcapApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(GuiJnetPcapApp.class, args);
+    }
+}
